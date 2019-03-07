afff35d65b825b55f8f0a45e455987ba69a2816b
Artem Gukov <artem.gukov@gmail.com>
2019-03-07 21:51:56 +0300

    Initial Commit

---
+++ TabBarProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TabBarProject
+//
+//  Created by Артем on 07/03/2019.
+//  Copyright © 2019 Gukov.space. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
